6dd0bc74c39d1012a9a4dc248b859a36bfdf4a1b
himynameismoose <mershellemae@gmail.com>
2022-05-03 15:33:10 -0700

    add doPost()

--- first-web-app/src/main/java/webapp/LoginServlet.java
+++ first-web-app/src/main/java/webapp/LoginServlet.java
@@ -39,6 +39,8 @@ public class LoginServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("name", request.getParameter("name"));
+		
 		request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
 	}
 }
