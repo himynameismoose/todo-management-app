ebad2de8c092f455d6778164ebe0b60421697893
himynameismoose <mershellemae@gmail.com>
2022-05-03 15:14:12 -0700

    Send post request to welcome.jsp

--- first-web-app/src/main/java/webapp/LoginServlet.java
+++ first-web-app/src/main/java/webapp/LoginServlet.java
@@ -34,13 +34,11 @@ public class LoginServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		request.setAttribute("name", request.getParameter("name"));
-
-		request.setAttribute("password", request.getParameter("password"));
-		
 		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
-
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
+	}
 }
